76c89d297df1705d1b76926e844191ea50e9165e
Caroline Longnecker <caroline@curieos.com>
2019-05-07 15:53:47 -0500

    Setup for PIO unit testing

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,9 +4,10 @@
  * @Email:  caroline@curieos.com
  * @Filename: main.cpp
  * @Last modified by:   curieos
- * @Last modified time: 2019-05-01T21:21:28-05:00
+ * @Last modified time: 2019-05-06T15:04:31-05:00
  */
 
+#ifndef UNIT_TEST
 
 #include <Arduino.h>
 #include "Machine.h"
@@ -18,3 +19,5 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+#endif
