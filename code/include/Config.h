89c035cabd853ad158d3bb1c7ab408e609793ab3
Caroline <caroline@curieos.com>
2019-07-11 18:21:21 -0500

    Add config values for the LCD and stepper drivers

--- code/include/Config.h
+++ code/include/Config.h
@@ -4,16 +4,29 @@
  * File Created: Monday, 27th May 2019 20:00:14
  * Author: Caroline (caroline@curieos.com)
  * -----
- * Last Modified: Monday, 27th May 2019 20:00:27
- * Modified By: Caroline (caroline@curieos.com>)
+ * Last Modified: Wednesday June 12th 2019 16:38:40
+ * Modified By: Caroline
  * -----
  * License: MIT License
  */
 
-#ifndef CONFIG
-#define CONFIG
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// Stepper Driver Section
+// Step/Dir
+// SPI
 #define DRIVE1_CS 21
 #define DRIVE2_CS 15
+
+// LCD Section
+// Touch Screen
+#define YP A12
+#define XM A13
+#define YM 7
+#define XP 5
+//SPI
+#define LCD_CS 10
+#define LCD_DC 9
 
 #endif
