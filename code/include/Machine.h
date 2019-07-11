6142401bb531e3c2afd4b2a309786487902097bf
Caroline <caroline@curieos.com>
2019-07-11 18:24:23 -0500

    Move stepper driver initalization to separate func

--- code/include/Machine.h
+++ code/include/Machine.h
@@ -4,14 +4,14 @@
  * File Created: Monday, 27th May 2019 19:58:23
  * Author: Caroline (caroline@curieos.com)
  * -----
- * Last Modified: Monday, 27th May 2019 20:00:31
- * Modified By: Caroline (caroline@curieos.com>)
+ * Last Modified: Wednesday June 12th 2019 16:50:11
+ * Modified By: Caroline
  * -----
  * License: MIT License
  */
 
-#ifndef MACHINE
-#define MACHINE
+#ifndef MACHINE_H
+#define MACHINE_H
 
 #include "Config.h"
 #include "AMIS30543.h"
@@ -20,6 +20,7 @@
 class Machine {
 public:
   Machine();
+  void init();
 private:
   AMIS30543* stepper1;
   AMIS30543* stepper2;
--- code/src/Machine.cpp
+++ code/src/Machine.cpp
@@ -4,8 +4,8 @@
  * File Created: Monday, 27th May 2019 19:18:21
  * Author: Caroline (caroline@curieos.com)
  * -----
- * Last Modified: Monday, 27th May 2019 20:01:41
- * Modified By: Caroline (caroline@curieos.com>)
+ * Last Modified: Wednesday June 12th 2019 16:49:55
+ * Modified By: Caroline
  * -----
  * License: MIT License
  */
@@ -15,7 +15,9 @@
 Machine::Machine() {
   stepper1 = new AMIS30543();
   stepper2 = new AMIS30543();
+}
 
+void Machine::init() {
   stepper1->init(DRIVE1_CS);
   stepper2->init(DRIVE2_CS);
   stepper1->setCurrentMilliamps(1500);
