041a8bca28c0377675a2af5f0f1b6cc66e825cee
Caroline <caroline@curieos.com>
2019-07-11 18:24:55 -0500

    Change header blocks to match other files

--- code/include/Motor.h
+++ code/include/Motor.h
@@ -4,14 +4,14 @@
  * File Created: Monday, 27th May 2019 19:18:21
  * Author: Caroline (caroline@curieos.com)
  * -----
- * Last Modified: Monday, 27th May 2019 20:00:57
- * Modified By: Caroline (caroline@curieos.com>)
+ * Last Modified: Wednesday May 29th 2019 17:15:23
+ * Modified By: Caroline
  * -----
  * License: MIT License
  */
 
-#ifndef MOTOR
-#define MOTOR
+#ifndef MOTOR_H
+#define MOTOR_H
 
 #include "Config.h"
 
